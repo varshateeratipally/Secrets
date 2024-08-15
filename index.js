068179648ee857d2bf59cf165db7bb57d70bc212
varshateeratipally <139103379+varshateeratipally@users.noreply.github.com>
2024-08-15 13:38:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
+
 
 import express from "express";
 import bodyParser from "body-parser";
